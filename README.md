aeb716bbf7ee865584cd56370b40230a9295b3e3
liuchunlong <363040645@qq.com>
2018-10-23 18:34:46 +0800

    1. libyuv ，交叉编译。平台：rk1108.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # libyuv-lib-rk1108
 libyuv-lib-rk1108, libyuv库。rk1108交叉编译。
+
+交叉编译环境变量配置：
+编辑 ~/.bashrc 文件，然后，执行命令 source ~/.bashrc
+
+export CVR_SDK_ROOT=/home/liu/rk1108/29_rk1108-cvr
+export PATH=$PATH:$CVR_SDK_ROOT/prebuilts/toolschain/usr/bin
+export PATH=$PATH:$CVR_SDK_ROOT/prebuilts/toolschain/usr/arm-rkcvr-linux-uclibcgnueabihf/bin
+
+编译说明：
+1. ndk_system: 包含NDK，ANDROID.mk 语法实现。
+2. rk_system: 存放，系统库及头文件。默认，自动会查找该路径。
+
+productConfigs.mk，说明：
+存放一些编译选项，一般不需要改。
---
+++ libyuv/config.mk
@@ -0,0 +1,19 @@
+IS_ANDROID_OS = false
+IS_NEED_SHARED_PTR = false
+IS_NEED_COMPILE_STLPORT = false
+IS_NEED_LINK_STLPORT = false
+IS_NEED_COMPILE_TINYXML2 = true
+IS_NEED_COMPILE_EXPAT = true
+IS_SUPPORT_ION = true
+IS_BUILD_TEST_APP = true
+IS_CAM_IA10_API = false
+IS_RK_ISP10 = false
+IS_USE_RK_V4L2_HEAD = true
+
+BASE_DIR ?= $(CURDIR)/out
+BUILD_OUPUT_EXTERNAL_LIBS:=$(BASE_DIR)/lib
+CROSS_COMPILE ?= arm-linux-
+TARGET_DIR ?= $(BASE_DIR)/system
+
+#CROSS_COMPILE ?= /extra/zyc/android6.0/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+#CROSS_COMPILE ?=
